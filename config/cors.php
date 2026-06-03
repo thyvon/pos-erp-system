655ab5824c63c9e807b2495cb854902c1b272937
Vun Thy <Vun.Thy@mjqe.edu.kh>
2026-06-03 17:25:33 +0700

    Update

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
         env('FRONTEND_URL', ''),
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://systemsolution.online',
         'http://127.0.0.1:5173',
         'http://localhost:4173',
         'http://127.0.0.1:4173',
