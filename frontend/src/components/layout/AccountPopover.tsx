eeb9328cfb893e0eb0e89397abe34c9ba78a13e7
thyvon <70102736+thyvon@users.noreply.github.com>
2026-06-08 15:51:38 +0700

    Use exact pixel radius in account popover items

--- frontend/src/components/layout/AccountPopover.tsx
+++ frontend/src/components/layout/AccountPopover.tsx
@@ -94,7 +94,7 @@ export default function AccountPopover({ avatarBorderColor, buttonSx }: AccountP
               border: (theme) => `solid 1px ${theme.palette.divider}`,
               '& .MuiMenuItem-root': {
                 typography: 'body2',
-                borderRadius: (theme) => theme.shape.borderRadius,
+                borderRadius: (theme) => `${theme.shape.borderRadius}px`,
               },
             },
           },
